cff720368ff87b5be2ce644c84d18d962ee31cd9
Fran314 <francesco.ghog@gmail.com>
2020-06-28 23:23:25 +0200

    Fine implementazione algoritmo di pathfinding
    Finita l'implementazione dell'algoritmo di pathfinding.
    Corretti alcuni bug.
    Commentato tutto il commentabile.
    
    Molto probabilmente NON sarà l'ultimo commit.

--- ProgettoASD/ListaOrdinata.h
+++ ProgettoASD/ListaOrdinata.h
@@ -1,5 +1,8 @@
 #pragma once
 
+// L'ora era tarda quando mi sono messo a commentare questo pezzo di
+//	codice e mi sono permesso qualche commento poco "ortodosso"
+
 #include<string>
 
 using namespace std;
@@ -22,6 +25,25 @@ public:
 		}
 		else
 		{
+			// Se la lista è vuota non c'è nessun minimo da ritornare,
+			//	ritorna -1.
+			// Tecnicamente non ci sarebbe da inserire questo check,
+			//	poiché per come è implementato l'algoritmo,
+			//	EstraiMinimo viene chiamato SEMPRE dopo l'aver
+			//	controllato che la lista non sia vuota, quindi non ci
+			//	imbatte mai in questo errore.
+			// Tuttavia per questioni di completezza e poiché si tratta
+			//	di pochissime righe di codice, ho deciso di
+			//	implementare comunque questo check.
+
+			// Qualcuno potrebbe obiettare che aggiungere questo enorme
+			//	commento di giustificazione di esistenza di questo if
+			//	else rende questo blocco non più semplicemente
+			//	"pochissime righe di codice".
+			// Beh, quel qualcuno avrebbe estremamente ragione, ma
+			//	ormai siamo qui, queste righe sono scritte e non si
+			//	torna più indietro.
+
 			return -1;
 		}
 	}
@@ -31,8 +53,20 @@ public:
 	void Inserisci(string priorità, int val)
 	{
 		ndl* temp = lista;
+		// Il motivo per cui esiste il check sulla priorità
+		//	dell'elemento in testa alla lista è che il blocco di codice
+		//	che gestisce l'inserimento dell'elemento in posizione
+		//	generica utilizza un puntatore (temp) all'elemento
+		//	PRECEDENTE all'elemento nella posizione dove andrà inserito
+		//	il nuovo elemento. Poiché il nodo in testa (è un bravo
+		//	cittadino rispettoso della legge, ovvero) non ha nessun
+		//	precedente, va trattato in modo diverso.
 		if (temp == NULL || priorità.compare(temp->priorità) < 0)
 		{
+			// Se la lista è vuota o se l'elemento da inserire ha
+			//	priorità alfabeticamente precedente a quella
+			//	dell'elemento in testa alla lista, inserisci questo
+			//	elemento in testa alla lista
 			ndl* nuovo_nodo = new ndl;
 			nuovo_nodo->priorità = priorità;
 			nuovo_nodo->val = val;
@@ -53,11 +87,16 @@ public:
 		}
 	}
 
+	// Rimuovi dalla lista, se esiste, l'elemento con la seguente
+	//	priorità
 	void Rimuovi(string priorità)
 	{
 		ndl* temp = lista;
 		if (temp != NULL)
 		{
+			// Il motivo per cui si tratta l'elemento in testa alla
+			//	lista in modo diverso dal resto degli elementi è
+			//	analogo a quello spiegato in Inserisci()
 			if (priorità.compare(temp->priorità) == 0)
 			{
 				lista = lista->successivo;
@@ -69,6 +108,23 @@ public:
 				{
 					temp = temp->successivo;
 				}
+				// Il while scorre la lista finché essa non finisce o
+				//	non si trova il primo valore con priorità uguale o
+				//	(alfabeticamente) successiva a quella ricercata.
+				// Infatti:
+				//	- se la lista finisce, non serve cercare altrove
+				//	- se abbiamo trovato un elemento con priorità
+				//		uguale a quella cercata, non serve cercare
+				//		altrove
+				//	- se abbiamo trovato un elemento con priorità
+				//		successiva a quella cercata, poiché gli
+				//		elementi sono ordinati per priorità, sappiamo
+				//		che sicuramente non esiste alcun elemento con
+				//		la priorità cercata, quindi non serve cercare
+				//		altrove
+
+				// Controlliamo ora il motivo di arresto e nel caso sia
+				//	il secondo, eliminiamo l'elemento
 				if (temp->successivo != NULL && priorità.compare(temp->successivo->priorità) == 0)
 				{
 					ndl* temp2 = temp->successivo;
@@ -79,22 +135,11 @@ public:
 		}
 	}
 
+	// Ritorna true se la lista è vuota, false altrimenti
 	bool Vuota()
 	{
 		return (lista == NULL);
 	}
-
-	/*
-	void Stampa()
-	{
-		ndl* temp = lista;
-		while (temp != NULL)
-		{
-			cout << temp->priorità << ", " << temp->val << endl;
-			temp = temp->successivo;
-		}
-	}
-	*/
 
 private:
 	// Struct contenente il singolo nodo della lista (ndl)
--- ProgettoASD/Main.cpp
+++ ProgettoASD/Main.cpp
@@ -174,80 +174,169 @@ void inizializzaGrafi()
 //	in una stringa della forma hh:mm:ss
 string intToTimeString(int time)
 {
-	int hours = time / 3600;
-	int minutes = (time / 60) % 60;
-	int seconds = time % 60;
+	int ore = time / 3600;
+	int minuti = (time / 60) % 60;
+	int secondi = time % 60;
 
-	string to_return = "";
+	string da_ritornare = "";
 
-	if (hours >= 10) to_return += to_string(hours) + ":";
-	else to_return += "0" + to_string(hours) + ":";
+	if (ore >= 10) da_ritornare += to_string(ore) + ":";
+	else da_ritornare += "0" + to_string(ore) + ":";
 
-	if (minutes >= 10) to_return += to_string(minutes) + ":";
-	else to_return += "0" + to_string(minutes) + ":";
+	if (minuti >= 10) da_ritornare += to_string(minuti) + ":";
+	else da_ritornare += "0" + to_string(minuti) + ":";
 
-	if (seconds >= 10) to_return += to_string(seconds);
-	else to_return += "0" + to_string(seconds);
+	if (secondi >= 10) da_ritornare += to_string(secondi);
+	else da_ritornare += "0" + to_string(secondi);
 
-	return to_return;
+	return da_ritornare;
+}
+
+// Funzione che trasforma un qualsiasi numero in una stringa di almeno
+//	5 caratteri, eventualmente riempiendo gli spazi a sinistra con
+//	degli zeri. Questo serve per la priorità degli elementi nella coda
+//	dell'algoritmo di pathfinding, poiché questi vengono ordinati
+//	principalmente rispetto ad un tempo scritto in secondi in una
+//	stringa e ordinati in ordine alfabetico. C'è bisogno di unificare
+//	la quantità di cifre poiché altrimenti la stringa 200, che
+//	rappresenta il momento nel tempo 200 secondi dopo la mezzanotte
+//	(00:03:20), verrebbe DOPO la stringa 10000, che rappresenta il
+//	momento nel tempo 10000 secondi dopo la mezzanotte (02:46:40),
+//	ma PRIMA di 3000 (ovvero 00:50:00) nonostante siano ENTRAMBI dopo
+//	temporalmente, creado quindi un ordine non corrispondente a quello
+//	temporale che a noi interessa.
+//	Le stringhe 00200, 03000 e 10000 sono invece ugualmente ordinate
+//	temporalmente e alfabeticamente
+string intTo5DigitString(int arg)
+{
+	if (arg < 10) return "0000" + to_string(arg);
+	if (arg < 100) return "000" + to_string(arg);
+	if (arg < 1000) return "00" + to_string(arg);
+	if (arg < 10000) return "0" + to_string(arg);
+	return to_string(arg);
+	// N.B: se l'argomento avesse PIU' di 5 cifre, anche l'output
+	//	avrebbe più di 5 caratteri. Tuttavia, questa funzione verrà
+	//	chiamata solo su numeri con al più 5 cifre, quindi non c'è
+	//	bisogno di gestire questo tipo di "errore". Allo stesso modo,
+	//	la funzione non si preoccupa di eventuali argomenti negativi
+	//	poiché verrà chiamata solo su numeri positivi.
 }
 
 //--- TFS = Time First Search ---//
 // L'algoritmo, seppur l'avessi pensato originariamente come una
 //	modifica del BFS (da cui il nome TFS), è in realtà una versione
-//	riadattata dell'algoritmo di Dijkstra
+//	riadattata dell'algoritmo di Dijkstra.
+// La differenza principale dall'algoritmo di Dijkstra consiste nel
+//	peso degli archi: se in Dijkstra il peso dell'arco è costante e
+//	noto a priori, nel nostro caso il peso del singolo arco sarà
+//	composto da una parte costante (la durata del percorso) e da una
+//	parte variabile dipendente dal contesto (il tempo di attesa da
+//	quando si arriva su un nodo a quando si prende il mezzo pubblico
+//	del relativo arco).
+// Questo potrebbe sembrare un problema, ma può essere risolto in modo
+//	molto semplice: osserviamo innanzitutto che nell'algoritmo di
+//	Dijkstra, supponendo di disporre di un altro modo per calcolare la
+//	distanza dal generico nodo alla sorgente, non è fondamentale sapere
+//	il peso degli archi, e quindi nel nostro caso non sarebbe
+//	necessario calcolare il tempo di attesa. Inoltre, un'altra cosa che
+//	è lecita fare e che non previene l'algoritmo dal funzionare
+//	correttamente, è incrementare tutte le distanze dei vari nodi di
+//	una costante positiva, in modo che la sorgente si trovi a
+//	"distanza K" da se stesso invece che a distanza 0, e tutti gli
+//	altri a distanza d+K. Questa modifica fa comunque funzionare
+//	correttamente l'algoritmo poiché l'unica cosa che importa non è la
+//	distanza effettiva ma l'ordine tra le distanze.
+// Fatte queste premesse, immaginiamo ora di incrementare tutte le
+//	distanze di [tempo_iniziale]. Così facendo, i nodi adiacenti si
+//	troveranno a distanza ["tempo di attesa"]+["durata della tratta"]
+//	+[tempo_iniziale]. Osserviamo però che indipendentemente dall'arco,
+//	la somma di questi tre valori corrisponde sempre al arr_time
+//	dell'arco con il quale si raggiunge il nodo. Allora disponiamo di
+//	modo di calcolare la distanza senza dover necessariamente sapere
+//	il tempo di attesa per la singola tratta, potendo usare
+//	semplicemente il valore di arr_time come distanza del nodo dalla
+//	sorgente.
 void TFS(int nodo_iniziale, int nodo_finale, int tempo_iniziale, int tempo_massimo)
 {
-	etichetta_nodo* nodi = new etichetta_nodo[N];
-	nodi[nodo_iniziale].arr_time = tempo_iniziale;
+	etichetta_nodo* etichette = new etichetta_nodo[N];
+	etichette[nodo_iniziale].arr_time = tempo_iniziale;
+	// Tanto quanto in Dijkstra settiamo la distanza del primo nodo a
+	//	0, qui settiamo la distanza del primo nodo a [tempo_iniziale].
+	// Il perché è spiegato sopra questa funzione.
 
 	ListaOrdinata coda;
-	coda.Inserisci(to_string(tempo_iniziale) + "-" + to_string(nodo_iniziale), nodo_iniziale);
+	coda.Inserisci(intTo5DigitString(tempo_iniziale) + "-" + to_string(nodo_iniziale), nodo_iniziale);
+	// La priorità di ogni elemento della lista non è data SOLO dal
+	//	tempo di arrivo (come spiegato sopra questa funzione) ma dal
+	//	tempo di arrivo e dall'indice del corrispondente nodo. Questo
+	//	perché per semplicità di codice, si suppone che ogni elemento
+	//	della lista ordinata abbia priorità unica tra le altre. Poiché
+	//	però è possibile che due nodi abbiamo la stessa distanza in
+	//	tempo dal nodo iniziale, ci assicuriamo l'unicità della
+	//	priorità aggiungendo l'indice del nodo (infatti ogni nodo
+	//	compare al più una volta nella coda, casomai cambiando
+	//	posizione). La scelta di mettere un trattino tra i due valori
+	//	è puramente cosmesi estetica dei dati.
 
-	bool guardia = true;
-
-	while (!coda.Vuota() && !nodi[nodo_finale].visitato)
+	while (!coda.Vuota() && !etichette[nodo_finale].visitato)
 	{
 		int u = coda.EstraiMinimo();
-		nodi[u].visitato = true;
-		if(nodi[u].arr_time - tempo_iniziale> tempo_massimo) break;
+		etichette[u].visitato = true;
+		if(etichette[u].arr_time - tempo_iniziale> tempo_massimo) break;
+
+		// Gli archi rappresentanti trasporti pubblici e gli archi
+		//	rappresentanti camminate vengono trattati in modo
+		//	differente poiché nel secondo caso non esiste nessun tempo
+		//	di attesa (a meno che non siate pigri e vogliate riprendere
+		//	fiato, ma in quel caso state guardando l'algoritmo
+		//	sbagliato. Questo algoritmo è solo per veri camminatori
+		//	esperti e instancabili), e va quindi calcolato il tempo di
+		//	arrivo al nodo successivo sommando il tempo di arrivo al
+		//	nodo attuale e la durata della camminata.
 		for (arco_pt a : grafo_pt[u])
 		{
-			if (a.dep_time >= nodi[u].arr_time)
+			// Considera l'arco solo se il suo tempo di partenza è
+			//	successivo al tempo di arrivo al nodo attuale.
+			// Se così non fosse, sarebbe impossibile prendere il
+			//	relativo mezzo pubblico.
+			if (a.dep_time >= etichette[u].arr_time)
 			{
-				if (!nodi[a.to_stop].visitato && a.arr_time < nodi[a.to_stop].arr_time)
+				if (!etichette[a.to_stop].visitato && a.arr_time < etichette[a.to_stop].arr_time)
 				{
-					coda.Rimuovi(to_string(nodi[a.to_stop].arr_time) + "-" + to_string(a.to_stop));
-					nodi[a.to_stop].arr_time = a.arr_time;
-					nodi[a.to_stop].last_node = u;
-					nodi[a.to_stop].route_type = a.route_type;
-					nodi[a.to_stop].dep_time = a.dep_time;
-					coda.Inserisci(to_string(a.arr_time) + "-" + to_string(a.to_stop), a.to_stop);
+					coda.Rimuovi(intTo5DigitString(etichette[a.to_stop].arr_time) + "-" + to_string(a.to_stop));
+					etichette[a.to_stop].arr_time = a.arr_time;
+					etichette[a.to_stop].last_node = u;
+					etichette[a.to_stop].route_type = a.route_type;
+					etichette[a.to_stop].dep_time = a.dep_time;
+					coda.Inserisci(intTo5DigitString(a.arr_time) + "-" + to_string(a.to_stop), a.to_stop);
 				}
 			}
 		}
 		for (arco_walk a : grafo_walk[u])
 		{
-			int arr_time = nodi[u].arr_time + a.d_walk;
+			int arr_time = etichette[u].arr_time + a.d_walk;
+			// Calcolo del tempo di arrivo al nodo successivo sommando
+			//	il tempo di arrivo al nodo attuale e la durata della
+			//	camminata.
 
-			if (!nodi[a.to_stop].visitato && arr_time < nodi[a.to_stop].arr_time)
+			if (!etichette[a.to_stop].visitato && arr_time < etichette[a.to_stop].arr_time)
 			{
-				coda.Rimuovi(to_string(nodi[a.to_stop].arr_time) + "-" + to_string(a.to_stop));
-				nodi[a.to_stop].arr_time = arr_time;
-				nodi[a.to_stop].last_node = u;
-				nodi[a.to_stop].route_type = -1;
-				nodi[a.to_stop].dep_time = nodi[u].arr_time;
-				coda.Inserisci(to_string(arr_time) + "-" + to_string(a.to_stop), a.to_stop);
+				coda.Rimuovi(intTo5DigitString(etichette[a.to_stop].arr_time) + "-" + to_string(a.to_stop));
+				etichette[a.to_stop].arr_time = arr_time;
+				etichette[a.to_stop].last_node = u;
+				etichette[a.to_stop].route_type = -1;
+				etichette[a.to_stop].dep_time = etichette[u].arr_time;
+				coda.Inserisci(intTo5DigitString(arr_time) + "-" + to_string(a.to_stop), a.to_stop);
 			}
 		}
 	}
 
 	int x = nodo_finale;
-	while (nodi[x].last_node != -1)
+	while (etichette[x].last_node != -1)
 	{
-		cout << "[" << intToTimeString(nodi[x].dep_time) << "-" << intToTimeString(nodi[x].arr_time) << "] da ";
-		cout << nodi[x].last_node << " a " << x;
-		switch (nodi[x].route_type)
+		cout << "[" << intToTimeString(etichette[x].dep_time) << "-" << intToTimeString(etichette[x].arr_time) << "] da ";
+		cout << etichette[x].last_node << " a " << x;
+		switch (etichette[x].route_type)
 		{
 		case -1:
 			cout << " camminando (walk)";
@@ -278,7 +367,7 @@ void TFS(int nodo_iniziale, int nodo_finale, int tempo_iniziale, int tempo_massi
 			break;
 		}
 		cout << endl;
-		x = nodi[x].last_node;
+		x = etichette[x].last_node;
 	}
 }
 
@@ -288,10 +377,19 @@ int main()
 
 	int nodo_iniziale, nodo_finale, tempo_iniziale, tempo_massimo;
 
+	// Il motivo di questo ciclo while è dato dalla lentezza di
+	//	caricamento dei dati iniziali.
+	// L'algoritmo di pathfinding è relativamente veloce, ma il tempo
+	//	di lettura dei dati (quantomeno quelli relativi a Parigi) si
+	//	aggira attorno al minuto sul mio computer, che è davvero tanto.
+	// Quindi, nel caso si volesse testare il programma con più di un
+	//	percorso, questo ciclo while permette di testarne più di uno
+	//	senza dover ricaricare i dati degli archi.
 	while (true)
 	{
-		cout << "Inserire nodo iniziale: ";
+		cout << "Inserire nodo iniziale (o inserire -1 per uscire): ";
 		cin >> nodo_iniziale;
+		if (nodo_iniziale == -1) break;
 		cout << "Inserire nodo finale: ";
 		cin >> nodo_finale;
 		cout << "Inserire tempo di partenza (in termini di secondi dalla mezzanotte, da 0 a 86399): ";
